9dc8cb4a2ac497f1684b8e4e836ed133bbdfd2cc
Mutsa Taringa <mutsahfx@gmail.com>
2025-06-25 00:42:50 +0200

    Create paymentAPI.jsx

---
+++ src/api/paymentAPI.jsx
@@ -0,0 +1,34 @@
+import { toast } from 'react-toastify';
+import axios from 'axios';
+
+const URL = 'http://localhost:3000/payment/';
+
+export async function handlePaynow(
+  reference,
+  orderItems,
+  total,
+  totalCartAmount,
+  shippingFee,
+  taxAmount,
+) {
+  try {
+    const response = await axios.post(`${URL}create-order`, {
+      email: 'taringamutsah@gmail.com',
+      reference: reference,
+      totalAmount: total,
+      subTotal: totalCartAmount,
+      shippingFee: shippingFee,
+      tax: taxAmount,
+      orderItems: orderItems,
+    });
+
+    if (response.data.success) {
+      window.location.href = response.data.redirectUrl;
+    } else {
+      toast.error('Failed to start payment');
+    }
+  } catch (error) {
+    console.error(error);
+    toast.error('Error processing payment');
+  }
+}
