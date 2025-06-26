89422c40ab5e2f498cb26014349673eca4a6ff0a
Mutsa Taringa <mutsahfx@gmail.com>
2025-06-27 01:31:13 +0200

    Update UserOrders.jsx

--- src/_root/user/pages/UserOrders.jsx
+++ src/_root/user/pages/UserOrders.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useMemo, useState } from 'react';
 import { ShopContext } from '../../../context/ShopContext';
 import OrdersTab from '../../../components/OrdersTab';
 import Modal from '../../../components/Modal';
