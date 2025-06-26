8b864c0400770c8fc22f1c54564efa51a267f742
Mutsa Taringa <mutsahfx@gmail.com>
2025-06-26 16:39:51 +0200

    Create slide.js

---
+++ src/partials/slide.js
@@ -0,0 +1,24 @@
+import MacBook from '../assets/3.jpg';
+import Office from '../assets/4.jpg';
+import PS5 from '../assets/6.jpg';
+
+export const slides = [
+  {
+    title: 'Power Meets Elegance - Apple MacBook Pro is Here for you!',
+    subtitle: 'Exclusive Deal 40% Off',
+    image: MacBook,
+    cta: 'Shop Now',
+  },
+  {
+    title: 'Unleash Productivity - Get the Ultimate Office Setup!',
+    subtitle: 'Limited Time Offer',
+    image: Office,
+    cta: 'Shop Now',
+  },
+  {
+    title: 'Game On - Elevate Your Gaming with the Best Headphones',
+    subtitle: 'Gaming Sale Now Live!',
+    image: PS5,
+    cta: 'Shop Now',
+  },
+];
