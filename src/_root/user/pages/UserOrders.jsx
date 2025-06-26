6195c0afa746c1ca4e00303e68ba0ff7adc249e4
Mutsa Taringa <mutsahfx@gmail.com>
2025-06-27 00:25:23 +0200

    Update UserOrders.jsx

--- src/_root/user/pages/UserOrders.jsx
+++ src/_root/user/pages/UserOrders.jsx
@@ -1,4 +1,27 @@
+import { useContext } from 'react';
+import { ShopContext } from '../../../context/ShopContext';
+import OrdersTab from '../../../components/OrdersTab';
+
 const UserOrders = () => {
-  return <div className="container py-8">UserOrders</div>;
+  const { orders } = useContext(ShopContext);
+
+  const openModal = (type, item = null) => {
+    setModalType(type);
+    setSelectedProduct(item);
+    setSelectedOrder(item);
+    setShowModal(true);
+  };
+
+  return (
+    <div className="container py-8">
+      <div className="mb-6">
+        <h3 className="font-semibold text-2xl">All Products</h3>
+        <div className="ruler w-full h-px bg-gray-200"></div>
+      </div>
+      <div className="">
+        <OrdersTab orders={orders} onOpenModal={openModal} />
+      </div>
+    </div>
+  );
 };
 export default UserOrders;
