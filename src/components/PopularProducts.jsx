1ee33bdcd252b47b76a5527f5f36d54700e7092d
Mutsa Taringa <mutsahfx@gmail.com>
2025-06-27 02:05:06 +0200

    Update PopularProducts.jsx

--- src/components/PopularProducts.jsx
+++ src/components/PopularProducts.jsx
@@ -32,7 +32,7 @@ export const PopularProducts = () => {
         <div className="ruler"></div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-full">
-        {products.slice(0, 3).map((product) => (
+        {products.slice(0, 4).map((product) => (
           <ProductCard key={product.id} product={product} imageUrl={imageUrls[product.id]} />
         ))}
       </div>
