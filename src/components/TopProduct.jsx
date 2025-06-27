e34f7266af460f480a7307cf6f4c942a590adc3d
Mutsa Taringa <mutsahfx@gmail.com>
2025-06-27 09:14:20 +0200

    Update TopProduct.jsx

--- src/components/TopProduct.jsx
+++ src/components/TopProduct.jsx
@@ -1,4 +1,3 @@
-import { ArrowRight } from 'lucide-react';
 import { getTopProduct } from '../partials/products';
 
 export const TopProduct = () => {
@@ -6,11 +5,8 @@ export const TopProduct = () => {
     <div className="bg-[#e9eff8] mt-12 rounded-lg py-12 ">
       {getTopProduct.map((product, key) => (
         <div className="flex flex-col items-center" key={key}>
-          <h3 className="font-semibold text-2xl">{product.title}</h3>
+          <h3 className="font-semibold text-2xl text-center">{product.title}</h3>
           <p className="w-[60%] text-center py-3">{product.description}</p>
-          {/* <button className="bg-primary text-white mt-2 py-1 px-5 flex items-center gap-2 rounded-sm">
-            Buy Now <ArrowRight size={16}></ArrowRight>
-          </button> */}
         </div>
       ))}
     </div>
